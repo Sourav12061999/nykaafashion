0b2fd87abdd941ba09d7d4afe30060bf6a117ae1
Sourav Das <sdunity12061999@gmail.com>
2021-09-29 08:08:46 +0530

    Changed the font sizes a bit

--- nykaa-clone/womanpage/woman.js
+++ nykaa-clone/womanpage/woman.js
@@ -2,175 +2,175 @@ let data = [
   {
     brand: "Puma",
     name: "selena floral FileList",
-    price: "$2000",
+    price: "2000",
     size: "X,XX,XXL,",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/t/w/twentydressesmix_dr0935_1_984da863.jpg?rnd=20200526195200",
   },
   {
     brand: "Gap",
     name: "balck bella",
-    price: "$1347",
+    price: "1347",
     size: "X,M,L,XX,XXL,",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/1/4/146117301_1_7fcd0574.jpg?rnd=20200526195200",
   },
   {
     brand: "Puma",
     name: "chil pill supersoft palysuit",
-    price: "$799",
+    price: "799",
     size: "M,L,XX,XXS,",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/a/c/achwsivc466_1_a6c9da54.jpg?rnd=20200526195200",
   },
   {
     brand: "Gap",
     name: "plus size womens",
-    price: "$2000",
+    price: "2000",
     size: "X,M,L,XXS,XXL,",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/i/n/ind158-pnk_1_3dc181e4.jpg?rnd=20200526195200",
   },
   {
     brand: "Bady Moo",
     name: "balck bella",
-    price: "$1347",
+    price: "1347",
     size: "X,M,L,",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/p/h/pheeta-pht0067_1_96256fbd.jpg?rnd=20200526195200",
   },
   {
     brand: "Pepe jeans",
     name: "plus size womens",
-    price: "$979",
+    price: "979",
     size: "XXS,M,L,",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/o/_/o_228327501_1_857abfa6.jpg?rnd=20200526195200",
   },
   {
     brand: "Puma",
     name: "chil pill supersoft palysuit",
-    price: "$2000",
+    price: "2000",
     size: "XXL,M,L,",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/t/w/twentydressesmix_dr0769_1_90caf4b3.jpg?rnd=20200526195200",
   },
   {
     brand: "Gap",
     name: "chil pill supersoft palysuit",
-    price: "$1347",
+    price: "1347",
     size: "X,M,L,XX",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/k/a/kazo-123019black_1_85f6e006.jpg?rnd=20200526195200",
   },
   {
     brand: "Bady Moo",
     name: "balck bella",
-    price: "$976",
+    price: "976",
     size: "XS,M,L,XX",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/r/t/rtor0011_1_67335460.jpg?rnd=20200526195200",
   },
   {
     brand: "Gap",
     name: "woman cotton off white",
-    price: "$456",
+    price: "456",
     size: "X,L,XX",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/a/h/ahtufrgfe19523_1_e0416277.jpg?rnd=20200526195200"
   },
   {
     brand: "Bee bay",
     name: "woman cotton off white",
-    price: "$986",
+    price: "986",
     size: "M,L,XXS",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/a/c/achwkea595-1_dc090828.jpg?rnd=20200526195200",
   },
   {
     brand: "Kaniroot",
     name: "chill pill supersoft",
-    price: "$567",
+    price: "567",
     size: "S,M,L,XXl",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/2/3/238451501_1_43bf9c90.jpg?rnd=20200526195200",
   },
   {
     brand: "Bee bay",
     name: "Curev multi-Color",
-    price: "$765",
+    price: "765",
     size: "S,X,L,XXl",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/r/t/rtor0011_1_67335460.jpg?rnd=20200526195200",
   },
   {
     brand: "Gap",
     name: "Mega striple",
-    price: "$9999",
+    price: "9999",
     size: "S,M,X,XXL",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/o/_/o_119512401_1_e2d71be7.jpg?rnd=20200526195200",
   },
   {
     brand: "Puma",
     name: "balck bella",
-    price: "$1347",
+    price: "1347",
     size: "S,M,X,XXL",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/1/4/146117301_1_7fcd0574.jpg?rnd=20200526195200",
   },
   {
     brand: "Kaniroot",
     name: "chil pill supersoft palysuit",
-    price: "$799",
+    price: "799",
     size: "S,M,L,XXL",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/a/c/achwsivc466_1_a6c9da54.jpg?rnd=20200526195200",
   },
   {
     brand: "Puma",
     name: "plus size womens",
-    price: "$2000",
+    price: "2000",
     size: "S,XX",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/i/n/ind158-pnk_1_3dc181e4.jpg?rnd=20200526195200",
   },
   {
     brand: "Gap",
     name: "balck bella",
-    price: "$1347",
+    price: "1347",
     size: "S,M,XX,XXL",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/p/h/pheeta-pht0067_1_96256fbd.jpg?rnd=20200526195200",
   },
   {
     brand: "Bee bay",
     name: "plus size womens",
-    price: "$979",
+    price: "979",
     size: "M,XXL,XX",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/o/_/o_228327501_1_857abfa6.jpg?rnd=20200526195200",
   },
   {
     brand: "Puma",
     name: "chil pill supersoft palysuit",
-    price: "$2000",
+    price: "2000",
     size: "M,X,L,XXL",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/t/w/twentydressesmix_dr0769_1_90caf4b3.jpg?rnd=20200526195200",
   },
   {
     brand: "Puma",
     name: "chil pill supersoft palysuit",
-    price: "$1347",
+    price: "1347",
     size: "M,L,XXL",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/k/a/kazo-123019black_1_85f6e006.jpg?rnd=20200526195200",
   },
   {
     brand: "Gap",
     name: "balck bella",
-    price: "$976",
+    price: "976",
     size: "S,XX",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/r/t/rtor0011_1_67335460.jpg?rnd=20200526195200",
   },
   {
     brand: "Puma",
     name: "woman cotton off white",
-    price: "$456",
+    price: "456",
     size: "XXL",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/a/h/ahtufrgfe19523_1_e0416277.jpg?rnd=20200526195200"
   },
   {
     brand: "Gap",
     name: "woman cotton off white",
-    price: "$986",
+    price: "986",
     size: "S,X,L",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/a/c/achwkea595-1_dc090828.jpg?rnd=20200526195200",
   },
   {
     brand: "Kaniroot",
     name: "chill pill supersoft",
-    price: "$567",
+    price: "567",
     size: "S,XXL,XX",
     img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/2/3/238451501_1_43bf9c90.jpg?rnd=20200526195200",
   },
@@ -189,15 +189,15 @@ function ShowProducts(data) {
 
     let text = document.createElement("div");
     text.setAttribute("class", "card-text");
-    let h3 = document.createElement("h3");
-    h3.textContent = element.brand.toUpperCase();
-    text.appendChild(h3);
+    let h4 = document.createElement("h4");
+    h4.textContent = element.brand.toUpperCase();
+    text.appendChild(h4);
     let p = document.createElement("p");
     p.textContent = element.name;
     p.setAttribute("class", "card-text-detail")
     text.appendChild(p);
     let p2 = document.createElement("p");
-    p2.textContent = "Price:- " + element.price;
+    p2.textContent = "$" + element.price;
     p2.setAttribute("class", "card-text-price")
     text.appendChild(p2);
     card.appendChild(text);
