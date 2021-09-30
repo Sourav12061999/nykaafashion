7bc2edb7d6d9e68a726194996d9d7581afc37927
Sourav Das <sdunity12061999@gmail.com>
2021-09-30 18:23:49 +0530

    Payment Page Completed

--- nykaa-clone/Payment Page/PaymentPage2/Payment2.js
+++ nykaa-clone/Payment Page/PaymentPage2/Payment2.js
@@ -5,13 +5,22 @@ let detailCash=document.getElementById("details-cash");
 
 let payBtn=document.getElementById("payBtn");
 
+let p=[document.getElementById("p1"),document.getElementById("p2"),
+document.getElementById("p3"),document.getElementById("p4"),document.getElementById("p5")]
+
+let data=JSON.parse(localStorage.getItem("nykaa-cart"));
+let totalPrice=0;
+CartTotal();
+payBtn.textContent=`Pay $ ${totalPrice} Now`
+
+
 let detailCardBtn=document.getElementById("methods1");
 detailCardBtn.addEventListener("click",function(){
   detailCard.style.display="block";
   detailUPI.style.display="none";
   detailGpay.style.display="none";
   detailCash.style.display="none";
-  payBtn.textContent="Pay Now"
+  payBtn.textContent=`Pay $ ${totalPrice} Now`
 })
 
 let detailUPIBtn=document.getElementById("methods2");
@@ -20,7 +29,7 @@ detailUPIBtn.addEventListener("click",function(){
   detailUPI.style.display="block";
   detailGpay.style.display="none";
   detailCash.style.display="none";
-  payBtn.textContent="Pay Now"
+  payBtn.textContent=`Pay $ ${totalPrice} Now`;
 })
 
 let detailGpayBtn=document.getElementById("methods3");
@@ -29,7 +38,7 @@ detailGpayBtn.addEventListener("click",function(){
   detailUPI.style.display="none";
   detailGpay.style.display="block";
   detailCash.style.display="none";
-  payBtn.textContent="Pay Now"
+  payBtn.textContent=`Pay $ ${totalPrice} Now`;
 })
 
 let detailCashBtn=document.getElementById("methods4");
@@ -38,5 +47,22 @@ detailCashBtn.addEventListener("click",function(){
   detailUPI.style.display="none";
   detailGpay.style.display="none";
   detailCash.style.display="block";
-  payBtn.textContent="Pay After Delivery In Cash"
+  payBtn.textContent=`Pay $ ${totalPrice} After Delivery In Cash`
 })
+
+function CartTotal(){
+  data.forEach(element => {
+    totalPrice=totalPrice+Number(element.price);
+  });
+  p[0].textContent="$ "+totalPrice;
+  p[1].textContent="$ "+totalPrice;
+  p[2].textContent="$ "+0;
+  if(totalPrice>400){
+    p[3].textContent="$ "+0
+    p[4].textContent="$ "+totalPrice;
+  }else{
+    p[3].textContent="$ "+50;
+    p[4].textContent="$ "+(totalPrice+50);
+  }
+  
+}
