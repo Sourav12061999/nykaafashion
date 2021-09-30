86d4c389118e2c67438daee4fa8760cbe749e850
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-09-30 10:26:28 +0530

    created
    LandingPage.js for LandingPage.html

---
+++ nykaa-clone/LandingPage.js
@@ -0,0 +1,43 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  function openNav() {
+  document.getElementById("mySidebar").style.width = "370px";
+  }
+  function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  }
+
+  
+  var mybutton = document.getElementById("myBtn");
+  window.onscroll = function() 
+  {scrollFunction()
+  };
+  function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+   {
+           mybutton.style.display = "block";
+   }      
+  else
+  {
+           mybutton.style.display = "none";
+  }
+  }
+ function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  }
